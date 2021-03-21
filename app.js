694d48c8ebf40e7d5e54a6bf8a625cd292c57ef6
Round0000 <billybinche@gmail.com>
2021-03-21 11:28:33 +0100

    more lighthouse tweaks

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+// PWA Service Worker
+if (navigator.serviceWorker) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function (registration) {
+      console.log(
+        "ServiceWorker registration successful with scope:",
+        registration.scope
+      );
+    })
+    .catch(function (error) {
+      console.log("ServiceWorker registration failed:", error);
+    });
+}
+
 // Data storage
 let markedDates = {};
 
